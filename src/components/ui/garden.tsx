7bd43f07f472a5bf05e74f62fe83feb6caa3b2ed
simz14 <szozulakova@gmail.com>
2025-05-07 19:23:16 +0200

    feat: add lights

--- src/components/ui/garden.tsx
+++ src/components/ui/garden.tsx
@@ -21,9 +21,12 @@ export function Garden() {
           return renderer
         }}
       >
+        <hemisphereLight args={['#cfe3f2', '#3f6236', 0.95]} />
+        <directionalLight color="#ffeec2" intensity={2.6} position={[18, 26, 10]} />
+
         <mesh>
           <boxGeometry args={[1, 1, 1]} />
-          <meshNormalMaterial />
+          <meshLambertNodeMaterial color="#8a5c33" />
         </mesh>
       </Canvas>
     </section>
