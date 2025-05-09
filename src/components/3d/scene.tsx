634d0b85378672afb6af35ee983c40ce438bf5b1
simz14 <szozulakova@gmail.com>
2025-05-09 20:51:29 +0200

    feat: add ground plane

--- src/components/3d/scene.tsx
+++ src/components/3d/scene.tsx
@@ -8,7 +8,12 @@ export function Scene() {
       <hemisphereLight args={['#cfe3f2', '#3f6236', 0.95]} />
       <directionalLight color="#ffeec2" intensity={2.6} position={[18, 26, 10]} />
 
-      <mesh>
+      <mesh rotation-x={-Math.PI / 2}>
+        <planeGeometry args={[24, 24]} />
+        <meshLambertNodeMaterial color="#568f42" />
+      </mesh>
+
+      <mesh position-y={0.5}>
         <boxGeometry args={[1, 1, 1]} />
         <meshLambertNodeMaterial color="#8a5c33" />
       </mesh>
