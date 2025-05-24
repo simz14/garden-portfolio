845de01d56cd28d08791a507a4d096fe4a0832b0
simz14 <szozulakova@gmail.com>
2025-05-24 15:52:17 +0200

    feat: block the far edges with fence colliders

--- src/components/3d/landscape/fence.tsx
+++ src/components/3d/landscape/fence.tsx
@@ -1,11 +1,26 @@
+import { CuboidCollider, RigidBody } from '@react-three/rapier'
+import { gardenConfig, obstacleConfig } from '../../../config/garden'
+import { fenceConfig } from '../../../config/terrain'
+import { getPostEdge } from '../../../utils/fence'
 import { Posts } from './fence/posts'
 import { Rails } from './fence/rails'
+
+const half = gardenConfig.gridSize / 2
+const halfWall = obstacleConfig.wallHeight / 2
+const halfBarrier = fenceConfig.barrierThickness / 2
+
+const barrier = getPostEdge() + fenceConfig.postWidth / 2 - halfBarrier
 
 export function Fence() {
   return (
     <>
       <Posts />
       <Rails />
+
+      <RigidBody type="fixed" colliders={false}>
+        <CuboidCollider args={[half, halfWall, halfBarrier]} position={[0, halfWall, barrier]} />
+        <CuboidCollider args={[halfBarrier, halfWall, half]} position={[barrier, halfWall, 0]} />
+      </RigidBody>
     </>
   )
 }
