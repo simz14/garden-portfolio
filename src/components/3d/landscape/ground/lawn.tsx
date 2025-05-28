d9bbbd3a68430138c23187892cd9c13eb28a323b
simz14 <szozulakova@gmail.com>
2025-05-28 21:12:45 +0200

    refactor: paint the lawn with the grass texture

--- src/components/3d/landscape/ground/lawn.tsx
+++ src/components/3d/landscape/ground/lawn.tsx
@@ -1,16 +1,18 @@
 import { CuboidCollider, RigidBody } from '@react-three/rapier'
-import { gardenConfig, paletteConfig } from '../../../../config/garden'
+import { gardenConfig } from '../../../../config/garden'
 import { lawnConfig } from '../../../../config/terrain'
+import { useResources } from '../../../../context/resources'
 
 const halfGrid = gardenConfig.gridSize / 2
 const halfThickness = lawnConfig.colliderThickness / 2
 
 export function Lawn() {
+  const { ground } = useResources()
   return (
     <>
       <mesh rotation-x={-Math.PI / 2} position-y={lawnConfig.height} receiveShadow>
         <planeGeometry args={[gardenConfig.gridSize, gardenConfig.gridSize]} />
-        <meshLambertNodeMaterial color={paletteConfig.grass[0]} />
+        <meshLambertNodeMaterial map={ground} />
       </mesh>
 
       {/* Sunk so its top face lands on y=0, and no wider than the island, so
