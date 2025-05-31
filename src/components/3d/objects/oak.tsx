34677e331c6a931b4d27b665ce660e4d0942b0d3
simz14 <szozulakova@gmail.com>
2025-05-31 14:52:40 +0200

    feat: block walking through the oak

--- src/components/3d/objects/oak.tsx
+++ src/components/3d/objects/oak.tsx
@@ -1,9 +1,13 @@
+import { CylinderCollider, RigidBody } from '@react-three/rapier'
+import { obstacleConfig } from '../../../config/garden'
 import { oakConfig } from '../../../config/oak'
 import { getWorldOffset } from '../../../utils/garden'
 import { Branches } from './oak/branches'
 import { Canopy } from './oak/canopy'
 import { Leaves } from './oak/leaves'
 import { Trunk } from './oak/trunk'
+
+const halfWall = obstacleConfig.wallHeight / 2
 
 export function Oak() {
   return (
@@ -12,6 +16,18 @@ export function Oak() {
       <Branches />
       <Canopy />
       <Leaves />
+
+      <RigidBody type="fixed" colliders={false}>
+        <CylinderCollider args={[halfWall, oakConfig.collisionRadius]} position={[0, halfWall, 0]} />
+        <CylinderCollider
+          args={[halfWall, oakConfig.branchCollisionRadius]}
+          position={[
+            oakConfig.branchCollisionOffset,
+            halfWall,
+            -oakConfig.branchCollisionOffset,
+          ]}
+        />
+      </RigidBody>
     </group>
   )
 }
