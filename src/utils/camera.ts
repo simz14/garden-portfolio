bc095956a9be9dbfc457bc274fb52c77c206f3a6
simz14 <szozulakova@gmail.com>
2025-12-11 19:51:14 +0100

    refactor: drop the unused camera orbit helpers

--- src/utils/camera.ts
+++ src/utils/camera.ts
@@ -1,20 +1,6 @@
 import { MathUtils } from 'three'
 import { cameraConfig } from '../config/scene'
 
-export function getOrbitPosition(polarAngle: number, azimuth: number, distance: number) {
-  const sinPolar = Math.sin(polarAngle)
-
-  return [
-    distance * sinPolar * Math.sin(azimuth),
-    distance * Math.cos(polarAngle),
-    distance * sinPolar * Math.cos(azimuth),
-  ] as [number, number, number]
-}
-
-export function getHomePosition() {
-  return getOrbitPosition(cameraConfig.polarAngle, cameraConfig.azimuth, cameraConfig.distance)
-}
-
 export function getFittedZoom(width: number, height: number) {
   const widthZoom = width / cameraConfig.fitWidth
   const minHeightZoom = height / cameraConfig.maxFitHeight
